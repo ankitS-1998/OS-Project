aefc9fa038d5d50795a7fb5eb3ba7e70d95813ce
ankitS-1998 <sankit3119@gmail.com>
2019-03-26 23:26:04 +0530

    q2: finalize code

--- q2.c
+++ q2.c
@@ -8,7 +8,30 @@ struct process
 
     int flag;
 } p_list[100];
+void Sorting()
+{
+    struct process p;
 
+    int i, j;
+
+    for (i = 0; i < n - 1; i++)
+
+    {
+        for (j = i + 1; j < n; j++)
+
+        {
+            if (p_list[i].arrival_t > p_list[j].arrival_t)
+
+            {
+                p = p_list[i];
+
+                p_list[i] = p_list[j];
+
+                p_list[j] = p;
+            }
+        }
+    }
+}
 int main()
 {
     int i, t = 0, b_t = 0, peak;
